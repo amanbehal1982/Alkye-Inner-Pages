3c58be4486e509bb23303d202a5cb2b1e48f6dce
amanbehal1982 <amanbehal1982@gmail.com>
2020-07-30 22:38:39 +0530

    Worked on 'Our Work' page for Alkye Website

---
+++ assets/js/custom.js
@@ -0,0 +1,164 @@
+$(function() {
+    $('.project-content-slider').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        fade: true,
+        arrows: false,
+        asNavFor: '.project-image-slider'
+    });
+    $('.project-image-slider').slick({
+        autoplay: true,
+        autoplaySpeed: 3000,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        prevArrow: $('.prev'),
+        nextArrow: $('.next'),
+        dots: true,
+        appendDots: $('.slideControls'),
+        asNavFor: '.project-content-slider'
+    });
+    $('.project-content-slider2').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        fade: true,
+        arrows: false,
+        asNavFor: '.project-image-slider2'
+    });
+    $('.project-image-slider2').slick({
+        autoplay: true,
+        autoplaySpeed: 3000,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        prevArrow: $('.prev2'),
+        nextArrow: $('.next2'),
+        dots: true,
+        appendDots: $('.slideControls2'),
+        asNavFor: '.project-content-slider2'
+    });
+    $('.project-content-slider3').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        fade: true,
+        arrows: false,
+        asNavFor: '.project-image-slider3'
+    });
+    $('.project-image-slider3').slick({
+        autoplay: true,
+        autoplaySpeed: 3000,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        prevArrow: $('.prev3'),
+        nextArrow: $('.next3'),
+        dots: true,
+        appendDots: $('.slideControls3'),
+        asNavFor: '.project-content-slider3'
+    });
+    $('.project-content-slider4').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        fade: true,
+        arrows: false,
+        asNavFor: '.project-image-slider4'
+    });
+    $('.project-image-slider4').slick({
+        autoplay: true,
+        autoplaySpeed: 3000,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        prevArrow: $('.prev4'),
+        nextArrow: $('.next4'),
+        dots: true,
+        appendDots: $('.slideControls4'),
+        asNavFor: '.project-content-slider4'
+    });
+    $('.project-content-slider5').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        fade: true,
+        arrows: false,
+        asNavFor: '.project-image-slider5'
+    });
+    $('.project-image-slider5').slick({
+        autoplay: true,
+        autoplaySpeed: 3000,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        prevArrow: $('.prev5'),
+        nextArrow: $('.next5'),
+        dots: true,
+        appendDots: $('.slideControls5'),
+        asNavFor: '.project-content-slider5'
+    });
+    $('.project-content-slider6').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        fade: true,
+        arrows: false,
+        asNavFor: '.project-image-slider6'
+    });
+    $('.project-image-slider6').slick({
+        autoplay: true,
+        autoplaySpeed: 3000,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        prevArrow: $('.prev6'),
+        nextArrow: $('.next6'),
+        dots: true,
+        appendDots: $('.slideControls6'),
+        asNavFor: '.project-content-slider6'
+    });
+    $('.project-content-slider7').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        fade: true,
+        arrows: false,
+        asNavFor: '.project-image-slider7'
+    });
+    $('.project-image-slider7').slick({
+        autoplay: true,
+        autoplaySpeed: 3000,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        prevArrow: $('.prev7'),
+        nextArrow: $('.next7'),
+        dots: true,
+        appendDots: $('.slideControls7'),
+        asNavFor: '.project-content-slider7'
+    });
+    $('.project-content-slider8').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        fade: true,
+        arrows: false,
+        asNavFor: '.project-image-slider8'
+    });
+    $('.project-image-slider8').slick({
+        autoplay: true,
+        autoplaySpeed: 3000,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        prevArrow: $('.prev8'),
+        nextArrow: $('.next8'),
+        dots: true,
+        appendDots: $('.slideControls8'),
+        asNavFor: '.project-content-slider8'
+    });
+    $('.project-content-slider9').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        fade: true,
+        arrows: false,
+        asNavFor: '.project-image-slider9'
+    });
+    $('.project-image-slider9').slick({
+        autoplay: true,
+        autoplaySpeed: 3000,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        prevArrow: $('.prev9'),
+        nextArrow: $('.next9'),
+        dots: true,
+        appendDots: $('.slideControls9'),
+        asNavFor: '.project-content-slider9'
+    });
+});
